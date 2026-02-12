fccf509247b3b69822faaf2220d41babbeb12a4a
Ishita Sajeev <128696365+IshitaSajeev@users.noreply.github.com>
2026-02-13 00:23:24 +0530

    Add variables and input handling examples.

---
+++ input_and_casting.py
@@ -0,0 +1,10 @@
+"""
+Topic: Input Handling and Type Casting
+"""
+
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+
+next_year_age = user_age + 1
+
+print(f"Hello {user_name}, next year you will be {next_year_age} years old.")
