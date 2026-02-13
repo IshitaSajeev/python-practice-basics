03a1bd76aa92bfe3a3e767a07b353f04e8d2fdd8
Ishita Sajeev <128696365+IshitaSajeev@users.noreply.github.com>
2026-02-14 01:12:18 +0530

    Add basic loop examples using for and while

---
+++ loops_basic.py
@@ -0,0 +1,15 @@
+"""
+Basic loop examples
+"""
+
+# for loop
+print("For loop example:")
+for i in range(1, 6):
+    print(i)
+
+# while loop
+print("\nWhile loop example:")
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
