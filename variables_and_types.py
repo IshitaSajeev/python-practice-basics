bc4726535f9ad5045ac6c51dae67f470e432f2f7
Ishita Sajeev <128696365+IshitaSajeev@users.noreply.github.com>
2026-02-14 01:13:15 +0530

    Update variables_and_types.py

--- variables_and_types.py
+++ variables_and_types.py
@@ -2,20 +2,15 @@
 Topic: Variables and Data Types
 """
 
-# Basic variables
 name = "Alice"
 age = 21
 height = 5.4
 is_student = True
-
 print("Name:", name)
 print("Age:", age)
 print("Height:", height)
 print("Student:", is_student)
-
-# Type checking
 print(type(name), type(age), type(height), type(is_student))
 
-# Dynamic typing demonstration
 age = "twenty one"
 print("Updated age:", age, type(age))
